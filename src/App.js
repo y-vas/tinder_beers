307a1934b9517633719c5cb9a0a141ab6d27b3c6
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2021-08-05 09:41:33 +0200

    copy paste example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Card from './Components/Card.js';
+
+const cards = [{
+  	image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
+  	id: '1'
+	}, {
+  	image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
+  	id: '2'
+	}, {
+  	image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
+  	id: '3'
+	}, {
+  	image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
+  	id: '4'
+	}
+];
+
+function App (){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  	<div className='App'>
+  	{/*
+        Traversing through cards arrray using map function
+  	    and populating card with different image and color
+    */}
+
+  	{cards.map((card) => (
+  		<Card image={card.image} color={card.id} />
+  	))}
+
+  	</div>
   );
 }
 
